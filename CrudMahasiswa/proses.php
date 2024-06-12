c2365fefef7f6e461ad523edd9209bfd5831020a
sugih sahyudi <sugihsahyudi417@gmail.com>
2024-06-12 08:24:06 +0700

    first commit

---
+++ CrudMahasiswa/proses.php
@@ -0,0 +1,22 @@
+<?php
+include "koneksi.php";
+
+if (isset($_POST["proses"])) {
+    $nama_mhs = $_POST['input1'];
+    $prodi = $_POST['input2'];
+    $id = $_POST['input3'];
+    $proses = mysqli_query($koneksi, "INSERT INTO mahasiswa (nama_mhs, prodi, id) VALUES ('$nama_mhs', '$prodi', '$id')") or die(mysqli_error($koneksi));
+    $huruf_mutu = '';
+
+    if ($proses) {
+        echo "<script>alert('sukses');
+        window.location.href='form3.php'
+        </script>";
+    } else {
+        echo "<script>alert('gagal')
+        window.location.href='form3.php'
+        </script>";
+    }
+
+}
+?>
